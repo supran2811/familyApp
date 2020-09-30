b967aaf83b001596847a13d4016a5b8df512a3fb
supran2811 <supransengupta@gmail.com>
2020-09-30 07:49:13 +0530

    updated

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,7 +1,19 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
+import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(user?: { id: string; name: string; email: string }): string[];
+    }
+  }
+}
+
+jest.mock('../nats-wrapper');
+
 let mongo: any;
+
 beforeAll(async () => {
   mongo = new MongoMemoryServer();
   process.env.JWT_KEY = 'sfdsff';
@@ -23,3 +35,20 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = (
+  payload = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
+    name: 'test',
+  }
+) => {
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+
+  const sessionJSON = JSON.stringify(session);
+
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  return [`express:sess=${base64}`];
+};
